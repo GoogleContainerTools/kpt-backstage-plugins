e00ac128d5f8de2162aa08b496618e90dcf6bb5c
Christopher Fry <christopherfry@google.com>
2022-06-09 23:51:23 +0000

    refactor: simplify resource viewer selector logic

--- plugins/cad/src/components/ResourceViewerDialog/components/FirstClassViewerSelector.tsx
+++ plugins/cad/src/components/ResourceViewerDialog/components/FirstClassViewerSelector.tsx
@@ -21,12 +21,43 @@ import { getKptfileStructuredMetadata } from './FirstClassViewers/StructuredMeta
 import { getResourceQuotaStructuredMetadata } from './FirstClassViewers/StructuredMetadata/resources/resourceQuota';
 import { getRoleStructuredMetadata } from './FirstClassViewers/StructuredMetadata/resources/role';
 import { getRoleBindingStructuredMetadata } from './FirstClassViewers/StructuredMetadata/resources/roleBinding';
-import { StructuredMetadata } from './FirstClassViewers/StructuredMetadata/StructuredMetadata';
+import {
+  CustomMetadataFn,
+  StructuredMetadata,
+} from './FirstClassViewers/StructuredMetadata/StructuredMetadata';
 
 type FirstClassViewerSelectorProps = {
   apiVersion: string;
   kind: string;
   yaml: string;
+};
+
+const getCustomMetadataFn = (
+  groupVersionKind: string,
+): CustomMetadataFn | undefined => {
+  switch (groupVersionKind) {
+    case 'fn.kpt.dev/v1alpha1/ApplyReplacements':
+      return getApplyReplacementsStructuredMetadata;
+
+    case 'kpt.dev/v1/Kptfile':
+      return getKptfileStructuredMetadata;
+
+    case 'rbac.authorization.k8s.io/v1/Role':
+      return getRoleStructuredMetadata;
+
+    case 'rbac.authorization.k8s.io/v1/RoleBinding':
+      return getRoleBindingStructuredMetadata;
+
+    case 'v1/ConfigMap':
+      return getConfigMapStructuredMetadata;
+
+    case 'v1/ResourceQuota':
+      return getResourceQuotaStructuredMetadata;
+
+    default:
+  }
+
+  return undefined;
 };
 
 export const FirstClassViewerSelector = ({
@@ -36,63 +67,9 @@ export const FirstClassViewerSelector = ({
 }: FirstClassViewerSelectorProps) => {
   const groupVersionKind = `${apiVersion}/${kind}`;
 
-  switch (groupVersionKind) {
-    case 'fn.kpt.dev/v1alpha1/ApplyReplacements':
-      return (
-        <StructuredMetadata
-          yaml={yaml}
-          getCustomMetadata={getApplyReplacementsStructuredMetadata}
-        />
-      );
+  const customMetadataFn = getCustomMetadataFn(groupVersionKind);
 
-    case 'kpt.dev/v1/Kptfile':
-      return (
-        <StructuredMetadata
-          yaml={yaml}
-          getCustomMetadata={getKptfileStructuredMetadata}
-        />
-      );
-
-    case 'rbac.authorization.k8s.io/v1/Role':
-      return (
-        <StructuredMetadata
-          yaml={yaml}
-          getCustomMetadata={getRoleStructuredMetadata}
-        />
-      );
-
-    case 'rbac.authorization.k8s.io/v1/RoleBinding':
-      return (
-        <StructuredMetadata
-          yaml={yaml}
-          getCustomMetadata={getRoleBindingStructuredMetadata}
-        />
-      );
-
-    case 'v1/ConfigMap':
-      return (
-        <StructuredMetadata
-          yaml={yaml}
-          getCustomMetadata={getConfigMapStructuredMetadata}
-        />
-      );
-
-    case 'v1/Namespace':
-      return <StructuredMetadata yaml={yaml} />;
-
-    case 'v1/ServiceAccount':
-      return <StructuredMetadata yaml={yaml} />;
-
-    case 'v1/ResourceQuota':
-      return (
-        <StructuredMetadata
-          yaml={yaml}
-          getCustomMetadata={getResourceQuotaStructuredMetadata}
-        />
-      );
-
-    default:
-  }
-
-  return <StructuredMetadata yaml={yaml} />;
+  return (
+    <StructuredMetadata yaml={yaml} getCustomMetadata={customMetadataFn} />
+  );
 };
