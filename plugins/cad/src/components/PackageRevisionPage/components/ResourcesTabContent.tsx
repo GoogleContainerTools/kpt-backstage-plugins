7a86d8bbd1a3f01ea1b04111423d8f246d73f3d1
Christopher Fry <christopherfry@google.com>
2022-09-13 21:05:27 +0000

    feat: add resource filter options to package revision page

--- plugins/cad/src/components/PackageRevisionPage/components/ResourcesTabContent.tsx
+++ plugins/cad/src/components/PackageRevisionPage/components/ResourcesTabContent.tsx
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-import { makeStyles } from '@material-ui/core';
+import { Card, CardContent, makeStyles, TextField } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useMemo, useRef, useState } from 'react';
 import { PackageRevisionLifecycle } from '../../../types/PackageRevision';
 import { PackageRevisionResourcesMap } from '../../../types/PackageRevisionResource';
 import {
@@ -26,7 +26,10 @@ import {
 import { RevisionSummary } from '../../../utils/revisionSummary';
 import { Select } from '../../Controls';
 import { PackageRevisionPageMode } from '../PackageRevisionPage';
-import { PackageResourcesList } from './PackageResourcesList';
+import {
+  PackageResourceFilter,
+  PackageResourcesList,
+} from './PackageResourcesList';
 
 export type AlertMessage = {
   key: string;
@@ -49,9 +52,21 @@ type DiffSelectItem = {
   resourcesMap: PackageRevisionResourcesMap;
 };
 
+type ResourceFilterSelectItem = {
+  value: string;
+  label: string;
+  filter?: PackageResourceFilter;
+};
+
 const useStyles = makeStyles({
-  alert: {
-    marginBottom: '16px',
+  root: {
+    '& > *:not(:last-child)': {
+      marginBottom: '16px',
+    },
+  },
+  resourceViewOptions: {
+    display: 'flex',
+    gap: '16px',
   },
 });
 
@@ -60,6 +75,23 @@ const HIDE_COMPARISON_SELECT_ITEM: DiffSelectItem = {
   value: 'none',
   resourcesMap: {},
 };
+
+const RESOURCE_FILTER_OPTIONS: ResourceFilterSelectItem[] = [
+  {
+    label: 'Show all resources',
+    value: 'all-resources',
+  },
+  {
+    label: 'Show deployable resources only',
+    value: 'show-deployable-resources',
+    filter: r => !r.isLocalConfigResource,
+  },
+  {
+    label: 'Show local config resources only',
+    value: 'show-local-config-resources',
+    filter: r => r.isLocalConfigResource,
+  },
+];
 
 export const ResourcesTabContent = ({
   packageName,
@@ -78,10 +110,28 @@ export const ResourcesTabContent = ({
   const [diffSelection, setDiffSelection] = useState<DiffSelectItem>(
     HIDE_COMPARISON_SELECT_ITEM,
   );
+  const [searchString, setSearchString] = useState<string>('');
+  const [filterSelection, setFilterSelection] = useState<string>(
+    RESOURCE_FILTER_OPTIONS[0].value,
+  );
   const [baseResourcesMap, setBaseResourcesMap] =
     useState<PackageRevisionResourcesMap>();
 
   const lastPackageRevisionNameDiffSet = useRef<string>('');
+
+  const resourceFilter = useMemo<PackageResourceFilter>(() => {
+    const filterQuery: PackageResourceFilter =
+      RESOURCE_FILTER_OPTIONS.find(option => option.value === filterSelection)
+        ?.filter ?? (() => true);
+    const searchQuery: PackageResourceFilter =
+      searchString.length > 0
+        ? resource =>
+            resource.yaml.toLowerCase().includes(searchString.toLowerCase())
+        : () => true;
+
+    return packageResource =>
+      filterQuery(packageResource) && searchQuery(packageResource);
+  }, [filterSelection, searchString]);
 
   useEffect(() => {
     const packageRevisions = revisions.map(r => r.revision);
@@ -149,37 +199,56 @@ export const ResourcesTabContent = ({
   }, [diffSelection]);
 
   return (
-    <Fragment>
+    <div className={classes.root}>
       <Fragment>
         {alertMessages.map(alertMessage => (
-          <Alert
-            className={classes.alert}
-            severity="info"
-            key={`alert-${alertMessage.key}`}
-          >
+          <Alert severity="info" key={`alert-${alertMessage.key}`}>
             {alertMessage.message}
           </Alert>
         ))}
       </Fragment>
+
+      <Card>
+        <CardContent>
+          <div className={classes.resourceViewOptions}>
+            <TextField
+              label="Search"
+              variant="outlined"
+              value={searchString}
+              onChange={e => setSearchString(e.target.value)}
+              fullWidth
+            />
+
+            <Select
+              label="Resources Filter"
+              onChange={value => setFilterSelection(value)}
+              selected={filterSelection}
+              items={RESOURCE_FILTER_OPTIONS}
+            />
+
+            <Select
+              label="Compare Revision"
+              onChange={value =>
+                setDiffSelection(
+                  selectDiffItems.find(
+                    selectItem => selectItem.value === value,
+                  ) as DiffSelectItem,
+                )
+              }
+              selected={diffSelection.value}
+              items={selectDiffItems}
+            />
+          </div>
+        </CardContent>
+      </Card>
+
       <PackageResourcesList
         resourcesMap={resourcesMap}
         baseResourcesMap={baseResourcesMap}
         mode={mode}
+        resourceFilter={resourceFilter}
         onUpdatedResourcesMap={onUpdatedResourcesMap}
       />
-      <Select
-        label="Compare Revision"
-        onChange={value =>
-          setDiffSelection(
-            selectDiffItems.find(
-              selectItem => selectItem.value === value,
-            ) as DiffSelectItem,
-          )
-        }
-        selected={diffSelection.value}
-        items={selectDiffItems}
-        helperText="Compare revision to a previous revision or upstream blueprint."
-      />
-    </Fragment>
+    </div>
   );
 };
