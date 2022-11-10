c3aa05485e9fd5e33c100662c1bfdc0cca1484a5
Christopher Fry <christopherfry@google.com>
2022-11-10 05:23:46 +0000

    build: add config schema

---
+++ plugins/cad/config.d.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface Config {
+  /**
+   * Configuration for the Configuration as Data Plugin.
+   */
+  configAsData?: {
+    /**
+     * The namespace where Porch managed resources live.
+     */
+    resourcesNamespace: string;
+
+    /**
+     * Determines the GitOps delivery tool to use.
+     */
+    gitOpsDeliveryTool: 'none' | 'config-sync';
+
+    /**
+     * Determines where to receive the cluster configuration from.
+     */
+    clusterLocatorMethod: {
+      /**
+       * Determines how the client will locate the Kubernetes cluster.
+       */
+      type: 'current-context' | 'in-cluster';
+
+      /**
+       * Determines how the client will authenticate with the Kubernetes cluster.
+       */
+      authProvider: 'current-context' | 'google' | 'oidc' | 'service-account';
+
+      /**
+       * Optional. Determines the OIDC token provider to use when using the 'oidc' auth provider.
+       */
+      oidcTokenProvider?: 'google' | 'okta';
+
+      /**
+       * Optional. The service account token to be used when using the 'service-account' auth provider.
+       */
+      serviceAccountToken?: string;
+    };
+  };
+}
